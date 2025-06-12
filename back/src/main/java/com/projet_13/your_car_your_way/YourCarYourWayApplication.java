272e419aa6dec4fba3b39b5b43eca0ed8ae4caf1
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-06-12 09:36:20 +0200

    back: first commit

---
+++ back/src/main/java/com/projet_13/your_car_your_way/YourCarYourWayApplication.java
@@ -0,0 +1,13 @@
+package com.projet_13.your_car_your_way;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourCarYourWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourCarYourWayApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/projet_13/your_car_your_way/YourCarYourWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet_13.your_car_your_way;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourCarYourWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
