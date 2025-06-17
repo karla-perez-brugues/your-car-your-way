5fb2f30e921478536316c7889e330015dd253130
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-06-17 16:17:02 +0200

    back: create conversation status enum

---
+++ back/src/main/java/com/projet_13/your_car_your_way/enums/ConversationStatus.java
@@ -0,0 +1,8 @@
+package com.projet_13.your_car_your_way.enums;
+
+public enum ConversationStatus {
+    OPEN,      // La conversation est active et en cours
+    CLOSED,    // La conversation a été clôturée
+    PENDING,   // La conversation attend une action (ex: assignation admin)
+    ARCHIVED   // La conversation a été archivée
+}
