f5dcde65c851b759745d4e6514c2d540bd0cbf86
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-06-21 11:21:54 +0200

    back: add dtos

---
+++ back/src/main/java/com/projet_13/your_car_your_way/dto/UserDto.java
@@ -0,0 +1,102 @@
+package com.projet_13.your_car_your_way.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.projet_13.your_car_your_way.enums.UserType;
+
+import java.time.LocalDateTime;
+
+public class UserDto {
+    private Long id;
+    private String email;
+    private String phoneNumber;
+    private String lastName;
+    private String firstName;
+    private String password;
+    private boolean admin;
+    private UserType userType;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public UserDto() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
