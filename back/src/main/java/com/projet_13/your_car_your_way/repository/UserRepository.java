35b008dceb5959101228433a948bec98d4aed172
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-06-21 11:20:10 +0200

    back: update repositories

--- back/src/main/java/com/projet_13/your_car_your_way/repository/UserRepository.java
+++ back/src/main/java/com/projet_13/your_car_your_way/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.projet_13.your_car_your_way.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 }
