1dde9d3ad60e5d4ab99660fa9fbbe6e621b157b5
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-06-24 13:59:21 +0200

    front: add routing

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import {FrontOffice} from './features/front-office/front-office';
 import {LoginComponent} from './features/login/login.component';
+import {BackOffice} from './features/back-office/back-office';
+import {SingleConversation} from './features/single-conversation/single-conversation';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'front-office', component: FrontOffice },
+  { path: 'back-office', component: BackOffice },
+  { path: 'single-conversation', component: SingleConversation }, // fixme: add conversation id to path
 ];
