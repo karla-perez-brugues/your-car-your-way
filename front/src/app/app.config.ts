2e65e703bb456908bee0ece500ba9e739f00ea98
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-06-26 00:30:30 +0200

    front: remove ssr

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -2,15 +2,17 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import {provideHttpClient, withFetch} from '@angular/common/http';
+import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
+import {authenticationInterceptor} from '../interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authenticationInterceptor])
+    )
   ]
 };
