2af92c9fc58328ce652f5ca3e688b970719b1e74
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-06-26 10:43:47 +0200

    back: add jwt authentication

---
+++ back/src/main/java/com/p13/ycyw/service/UserService.java
@@ -0,0 +1,35 @@
+package com.p13.ycyw.service;
+
+
+import com.p13.ycyw.controller.payload.request.SignupRequest;
+import com.p13.ycyw.enums.UserType;
+import com.p13.ycyw.exception.NotFoundException;
+import com.p13.ycyw.model.User;
+import com.p13.ycyw.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AdminService adminService;
+
+    @Autowired
+    private CustomerService customerService;
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(NotFoundException::new);
+    }
+
+    public void create(SignupRequest signUpRequest) {
+        if (signUpRequest.getUserType() == UserType.ADMIN) {
+            this.adminService.create(signUpRequest);
+        } else {
+            this.customerService.create(signUpRequest);
+        }
+    }
+
+}
