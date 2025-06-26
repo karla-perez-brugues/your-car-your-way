bdb4f5019480446757f9239f722ecb8c56c887cd
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-06-26 18:36:22 +0200

    back: add exceptions when wrong type of user tries to access to resource

--- back/src/main/java/com/p13/ycyw/controller/ConversationController.java
+++ back/src/main/java/com/p13/ycyw/controller/ConversationController.java
@@ -2,9 +2,13 @@ package com.p13.ycyw.controller;
 
 import com.p13.ycyw.dto.ConversationDto;
 import com.p13.ycyw.dto.MessageDto;
+import com.p13.ycyw.enums.UserType;
+import com.p13.ycyw.exception.BadRequestException;
 import com.p13.ycyw.exception.NotFoundException;
 import com.p13.ycyw.model.Conversation;
+import com.p13.ycyw.model.User;
 import com.p13.ycyw.service.ConversationService;
+import com.p13.ycyw.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +23,9 @@ public class ConversationController {
     @Autowired
     private ConversationService conversationService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{id}")
     public ResponseEntity<ConversationDto> show(@PathVariable("id") String id) throws NotFoundException {
         Conversation conversation = conversationService.findById(Long.valueOf(id));
@@ -29,6 +36,12 @@ public class ConversationController {
 
     @PostMapping("")
     public ResponseEntity<ConversationDto> create(@Valid @RequestBody MessageDto messageDto, Principal principal) throws NotFoundException {
+        User user = userService.findByEmail(principal.getName());
+
+        if (user.getUserType() != UserType.CUSTOMER) {
+            throw new BadRequestException();
+        }
+
         Conversation conversation = conversationService.create(messageDto, principal.getName());
         ConversationDto conversationDto = conversationService.entityToDto(conversation);
 
--- back/src/main/java/com/p13/ycyw/controller/FrontOfficeHomeController.java
+++ back/src/main/java/com/p13/ycyw/controller/FrontOfficeHomeController.java
@@ -1,9 +1,13 @@
 package com.p13.ycyw.controller;
 
 import com.p13.ycyw.dto.ConversationDto;
+import com.p13.ycyw.enums.UserType;
+import com.p13.ycyw.exception.BadRequestException;
 import com.p13.ycyw.exception.NotFoundException;
 import com.p13.ycyw.model.Conversation;
+import com.p13.ycyw.model.User;
 import com.p13.ycyw.service.ConversationService;
+import com.p13.ycyw.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,10 +25,19 @@ public class FrontOfficeHomeController {
     @Autowired
     private ConversationService conversationService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("")
     public ResponseEntity<?> home(Principal principal) throws NotFoundException {
-        String currentEmail = principal.getName();
-        Optional<Conversation> conversation = conversationService.findByCustomer(currentEmail);
+        User user = userService.findByEmail(principal.getName());
+
+        if (user.getUserType() != UserType.CUSTOMER) {
+            throw new BadRequestException();
+        }
+
+        Optional<Conversation> conversation = conversationService.findByCustomer(user.getEmail());
+
         if (conversation.isPresent()) {
             ConversationDto conversationDto = conversationService.entityToDto(conversation.get());
             return new ResponseEntity<>(conversationDto, HttpStatus.OK);
